eddda5968a25073196bbd3252c8563fb2376f02f
mohdraza0403 <112565414+mohdraza0403@users.noreply.github.com>
2023-09-24 16:07:59 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import "./Home.css";
+import "./home.css";
 import {  Parallax } from "react-parallax";
 import Sdata from "./Sdata";
 import CommentSection from './CommentSection'; 
